e1f77b0e53253be39b1aa24fc6809e6ed11f2b6a
gustavonikov <gustavonikov@gmail.com>
2022-01-29 23:50:59 -0300

    style: identation changed to 2 spaces

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,5 +2,5 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('expect to render Card component', () => {
-    expect(shallow(<Card />)).toMatchSnapshot()
+  expect(shallow(<Card />)).toMatchSnapshot()
 })
